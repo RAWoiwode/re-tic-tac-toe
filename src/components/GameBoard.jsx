7fe0e8d92e905ec94ea334cddd0009ea9ae97a34
Ralph Woiwode <rawoiwode@gmail.com>
2023-12-06 09:11:52 -0600

    Creating base GameBoard

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+const GameBoard = () => {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  const handleClick = (row, col) => {
+    setGameBoard((prevGameBoard) => {
+      const updatedGameBoard = [...prevGameBoard.map((inner) => [...inner])];
+      updatedGameBoard[row][col] = "X";
+
+      return updatedGameBoard;
+    });
+  };
+
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((symbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleClick(rowIndex, colIndex)}>
+                  {symbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+};
+
+export default GameBoard;
